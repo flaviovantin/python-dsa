cff2c99abe87ea23f36906f04c712dcf52d72077
Flavio Vantin <flavio.vantin@gmail.com>
2023-02-27 23:43:02 -0500

    Implemented the append and prepend functions

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -1,48 +1,106 @@
 
-
 class Node:
 
     def __init__(self, value):
-        self.value = value
-        self.next: None
+        """ {'value': value, 'next': None} """
+        self.value = None if value is None else value
+        self.next = None
 
 
 class LinkedList:
 
-    def __init__(self, value):
-        node = Node(value)
+    def __init__(self, value=None):
+        if value is None:
+            node = None
+            self.length = 0
+        else:
+            node = Node(value)
+            self.length = 1
         self.head = node
         self.tail = node
-        self.length = 1
+
+    def print_list(self):
+        """ O(n) """
+        n = self.head
+        print('Head--> ' + str(self.head.value) if self.head is not None else 'Head--> None')
+        while n is not None:
+            print(n.value)
+            n = n.next
+        print('Tail--> ' + str(self.tail.value) if self.tail is not None else 'Tail--> None')
+        print('Length: ' + str(self.length) + '\n')
 
     def append(self, value):
-        # create a new node
+        """ O(1) """
         node = Node(value)
-        # add the node to end
-        pass
+        if self.head is None:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+        self.length += 1
 
     def pop(self):
+        """ O(n) """
         pass
 
     def prepend(self, value):
-        # create a new node
+        """ O(1) """
         node = Node(value)
-        # add the node to beginning
-        pass
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
 
     def remove(self, index, value):
+        """
+        at the beginning: O(1)
+        in the middle or at the end (pop): O(n)
+        """
         pass
 
     def insert(self, index, value):
-        # create a new node
-        node = Node(value)
-        # insert the node at the desire position
-        pass
+        """
+        at the beginning (prepend) or at the end (append): O(1)
+        in the middle: O(n)
+        """
+        if index == 0:
+            self.prepend(value)
+        elif index == self.length-1:
+            self.append(value)
+        else:
+            node = Node(value)
+
 
     def lookup(self, value):
+        """ O(n) """
         pass
 
 
-# (4)--> (23)--> (7)--> null
-my_linked_list = LinkedList(4)
+#  Head/Tail
+#    |  |
+#    V  V
+#    None
+my_linked_list = LinkedList()
+my_linked_list.print_list()
+#
+# my_linked_list.prepend(22)
+# my_linked_list.print_list()
 
+#  Head           Tail
+#   |              |
+#   V              V
+#  (4)--> (23)--> (7)--> null
+# my_linked_list = LinkedList(4)
+# my_linked_list.print_list()
+
+my_linked_list.append(23)
+my_linked_list.print_list()
+
+my_linked_list.append(7)
+my_linked_list.print_list()
+
+my_linked_list.prepend(11)
+my_linked_list.print_list()
